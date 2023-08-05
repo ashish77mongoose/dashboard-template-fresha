2e92158d653d2156abdaa2c195eca8a85747ab8b
Ashish Patel <ashish.mongoosetech@gmail.com>
2023-08-05 15:49:51 +0530

    first commit

---
+++ app/containers/pageListAsync.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import loadable from '../utils/loadable';
+import { SyncLoading } from '../components';
+
+export const Dashboard = loadable(() => import('./Dashboard'), {
+  fallback: <SyncLoading />,
+});
+export const Home = loadable(() => import('./Home'), {
+  fallback: <SyncLoading />,
+});
+export const SignIn = loadable(() => import('./UserSignUp/SignIn'), {
+  fallback: <SyncLoading />,
+});
---
+++ app/utils/constants.js
@@ -0,0 +1,55 @@
+import { reactIcons } from './icons';
+export const links = [
+  {
+    path: '/dashboard',
+    title: 'Home',
+    icon: reactIcons.home,
+  },
+  // {
+  //   path: '/sales',
+  //   title: 'Sales',
+  //   icon: reactIcons.sales,
+  // },
+
+  // {
+  //   path: '/calendar',
+  //   title: 'Calendar',
+  //   icon: reactIcons.calendar,
+  // },
+  // {
+  //   path: '/customers',
+  //   title: 'Customers',
+  //   icon: reactIcons.customer,
+  // },
+  // {
+  //   path: '/profile',
+  //   title: 'Online profile',
+  //   icon: reactIcons.person,
+  // },
+  // {
+  //   path: '/analytics',
+  //   title: 'Analytics',
+  //   icon: reactIcons.analytics,
+  // },
+  // {
+  //   path: '/discount-list',
+  //   title: 'Discount List',
+  //   icon: reactIcons.list,
+  // },
+
+  // {
+  //   path: '/services',
+  //   title: 'Services',
+  //   icon: reactIcons.services,
+  // },
+  // {
+  //   path: '/setup',
+  //   title: 'Setup',
+  //   icon: reactIcons.setup,
+  // },
+  // {
+  //   path: '/staff/employees',
+  //   title: 'Staff',
+  //   icon: reactIcons.staff,
+  // },
+];
